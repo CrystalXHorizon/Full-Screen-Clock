e57de1ff5af37609a03c61613c1d9c8b9967840f
EXPPlus3Paymon <expplus3paymon@users.noreply.github.com>
2026-05-24 21:56:22 +0800

    Remove temp build script

--- _git_cleanup.bat
+++ _git_cleanup.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "C:\Users\EXPPlus3Paymon\Documents\Codex\Full-Screen-Clock"
+del _git_push.bat
 git add -A
-git commit -m "Clean up temp build script"
+git commit -m "Remove temp build script"
 git push origin main
 echo Done
